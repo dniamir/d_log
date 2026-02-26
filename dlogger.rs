954e4eaa38bfe8116c833b8b5d94a4eb66a8213b
Darien Niamir <darien.niamir@gmail.com>
2026-02-25 19:56:42 -0800

    Allow logging with levels

--- dlogger.rs
+++ dlogger.rs
@@ -6,25 +6,30 @@ pub struct DLogger;
 pub static DLOGGER_HOLD_COUNT: AtomicU32 = AtomicU32::new(0);
 
 impl DLogger {
-#[inline]
+    #[inline]
     pub fn hold() {
-        #[cfg(not(feature = "no_hold"))]
+        // Always track depth regardless of feature [cite: 1761, 1803]
         DLOGGER_HOLD_COUNT.fetch_add(1, Ordering::Relaxed);
     }
 
     #[inline]
     pub fn release() {
-        // decrement, but don't allow underflow
-        #[cfg(not(feature = "no_hold"))]
         DLOGGER_HOLD_COUNT.fetch_update(Ordering::Relaxed, Ordering::Relaxed, |x| {
             if x > 0 { Some(x - 1) } else { Some(0) }
         }).ok();
     }
 
     #[inline]
+    pub fn get_hold_count() -> usize {
+        DLOGGER_HOLD_COUNT.load(Ordering::Relaxed) as usize
+    }
+
+    #[inline]
     pub fn allowed() -> bool {
-        // allowed only if counter == 0
-        DLOGGER_HOLD_COUNT.load(Ordering::Relaxed) == 0
+        #[cfg(feature = "no_hold")]
+        { true } // Always allow if feature is on [cite: 41, 1554]
+        #[cfg(not(feature = "no_hold"))]
+        { DLOGGER_HOLD_COUNT.load(Ordering::Relaxed) == 0 }
     }
 
     #[inline]
@@ -42,7 +47,42 @@ impl DLogger {
 macro_rules! d_info {
     ($($arg:tt)*) => {
         if $crate::d_log::dlogger::DLogger::allowed() {
-            defmt::info!($($arg)*);
+            #[cfg(feature = "no_hold")]
+            {
+                // 1. Change signature to take `defmt::Formatter` by value (it's Copy)
+                struct LogProxy<'a>(usize, &'a dyn Fn(defmt::Formatter<'_>));
+
+                impl<'a> defmt::Format for LogProxy<'a> {
+                    fn format(&self, fmt: defmt::Formatter<'_>) {
+                        // 2. Write the prefix to the formatter handle
+                        match self.0 {
+                            0 => {},
+                            1 => defmt::write!(fmt, "-"),
+                            2 => defmt::write!(fmt, "--"),
+                            3 => defmt::write!(fmt, "---"),
+                            4 => defmt::write!(fmt, "----"),
+                            5 => defmt::write!(fmt, "-------"),
+                            6 => defmt::write!(fmt, "---------"),
+                            7 => defmt::write!(fmt, "-----------"),
+                            8 => defmt::write!(fmt, "-------------"),
+                            9 => defmt::write!(fmt, "---------------"),
+                            _ => defmt::write!(fmt, "-----------------+"),
+                        };
+                        // 3. Call closure with the formatter handle directly
+                        (self.1)(fmt);
+                    }
+                }
+
+                let count = $crate::d_log::dlogger::DLogger::get_hold_count();
+                // 4. Pass `fmt` by value in the closure args
+                defmt::info!("{:?}", LogProxy(count, &|fmt| {
+                    defmt::write!(fmt, $($arg)*);
+                }));
+            }
+            #[cfg(not(feature = "no_hold"))]
+            {
+                defmt::info!($($arg)*);
+            }
         }
     };
 }
